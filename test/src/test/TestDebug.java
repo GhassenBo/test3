e45da3ca348ff7e2a4d9d8f8ed8e1c879ac2f249
GhassenBo <GhassenBo@192.168.1.5>
2016-06-17 08:44:45 +0200

    test1

---
+++ test/src/test/TestDebug.java
@@ -0,0 +1,12 @@
+package test;
+
+public class TestDebug {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String v;
+		v="fgfgfgfgf";	
+		System.out.println(v);
+	}
+
+}
