bc71b420901230f660df39b1731ee39bb1c18ced
GhassenBo <GhassenBo@192.168.1.5>
2016-06-17 09:08:17 +0200

    first change

--- test/src/test/TestDebug.java
+++ test/src/test/TestDebug.java
@@ -5,8 +5,10 @@ public class TestDebug {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String v;
+		String v1;
 		v="fgfgfgfgf";	
-		v="fgfgfgfgf";
+		v1="aaaaa";
+		System.out.println(v);
 		System.out.println(v);
 	}
 
